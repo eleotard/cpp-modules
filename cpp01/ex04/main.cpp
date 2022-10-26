eb47d937c808cc85fe6a04e81c62e22c5497f47f
elsie <eleotard@student.42.fr>
2022-10-26 13:19:27 +0200

    ex04 fini

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elsie <elsie@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:42:15 by elsie             #+#    #+#             */
-/*   Updated: 2022/10/24 18:38:34 by elsie            ###   ########.fr       */
+/*   Updated: 2022/10/25 16:17:54 by elsie            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,24 +28,24 @@ int	main(int argc, char **argv)
 		return (std::cout << "Could not open the input file" << std::endl, 2);
 		
 	std::string		replace_file_name((std::string)argv[1] + ".replace");
-	std::ofstream	ofs(replace_file_name, std::ios::app);
+	std::ofstream	ofs(replace_file_name.c_str(), std::ios::app);
 	if (!ofs.is_open()) 
 		return (std::cout << "Could not open the output file" << std::endl, 3);
+		
 	int	find_pos;
-	find_pos == 0;
+	find_pos = 0;
 	while (std::getline(ifs, temp))
 	{
+		find_pos = 0;
 		while (find_pos != -1)
 		{
 			find_pos = temp.find(s1, find_pos);
-			
+			if (find_pos != -1)
+			{
+				temp.erase(find_pos, s1.length());
+				temp.insert(find_pos, s2);
+			}
 		}
-		if (temp.compare(s1) == 0)
-			ofs << s2;
-		else
-			ofs << temp;
+		ofs << temp << std::endl;
 	}
-	
-	
-	
 }
